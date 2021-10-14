4266610239fce41ff50d3f5f2aac8632f800ce8c
Ana-Costa12 <79462547+Ana-Costa12@users.noreply.github.com>
2021-10-13 21:06:24 -0300

    Finalização

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = prompt("Digite seu nome!");
+let sobrenome = prompt("Digite seu sobrenome");
+
+let nomecompleto = `Seu nome completo é ${nome} ${sobrenome}`;
+let nomecatalogo = `Seu nome catálogo é ${sobrenome.toUpperCase()}, ${nome}`;
+//
+alert(nomecompleto);
+alert(nomecatalogo);
+
